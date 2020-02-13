95c65117efc123595dc9ba01a6aceb3689248c20
Samuel Escobar <sescobar8714@gmail.com>
2020-02-13 20:38:57 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,21 +1,67 @@
 def find_item_by_name_in_collection(name, collection)
   # Implement me first!
-  #
   # Consult README for inputs and outputs
+  i = 0
+  while i < collection.count
+    if collection[i][:item] == name
+      return collection[i]
+    end
+    i += 1
+  end
 end
 
 def consolidate_cart(cart)
   # Consult README for inputs and outputs
-  #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  new_array = []
+  i = 0
+  while i < cart.count
+  new_cart_item = find_item_by_name_in_collection(cart[i][:item], new_array)
+  if new_cart_item != nil
+    new_cart_item[:count] += 1
+  else
+    new_cart_item =
+    {
+      :item=>cart[i][:item],
+      :price=>cart[i][:price],
+      :clearance=>cart[i][:clearance],
+      :count=>1
+    }
+    new_array << new_cart_item
+  end
+i += 1
+  end
+  new_array
 end
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
-  #
   # REMEMBER: This method **should** update cart
-end
+  i = 0
+  while i < coupons.count
+cart_item = find_item_by_name_in_collection(coupons[i][:item], cart)
+coupon_item_name = "#{coupons[i][:item]} W/COUPON"
+cart_item_with_coupon = find_item_by_name_in_collection(coupon_item_name, cart)
+if cart_item && cart_item[:count] >= coupons[i][:num]
+  if cart_item_with_coupon
+    cart_item_with_coupon[:count] += coupons[i][:num]
+    cart_item[:count] -= coupons[i][:num]
+  else
+    cart_item_with_coupon = {
+      :item=>coupon_item_name,
+      :price=>coupons[i][:cost] / coupons[i][:num],
+      :count=>coupons[i][:num],
+      :clearance=>cart_item[:clearance]
+    }
+    cart << cart_item_with_coupon
+    cart_item[:count] -= coupons[i][:num]
+  end
+  end
+i += 1
+  end
+cart
+  end
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
